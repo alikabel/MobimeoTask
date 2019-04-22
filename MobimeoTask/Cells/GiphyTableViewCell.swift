17e7d2fd0a307fc6585158a9664f382b973de320
alikabel <alikabel1992@gmail.com>
2019-04-22 09:16:46 +0200

    Add GiphyTableViewCell

---
+++ MobimeoTask/Cells/GiphyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GiphyTableViewCell.swift
+//  MobimeoTask
+//
+//  Created by Ali Kabel on 22.04.19.
+//  Copyright © 2019 Ali Kabel. All rights reserved.
+//
+
+import UIKit
+
+class GiphyTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var giphyImageView: UIImageView?
+}
